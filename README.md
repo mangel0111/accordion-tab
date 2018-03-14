7227e2324093fef7ddb6178ab21fcb54c8c05cb1
Angel <Angel@Angels-MacBook-Pro.local>
2018-03-13 21:15:04 -0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Accordion - Tab
+This's a (HTML - CSS) Tab component where the wide of the screen is enough, otherwise the component change to a accordion.
+
+# To Start
+- `npm install`
+- `npm run start`
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	output: {
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{ from: './src/index.html' }
+		]),
+		new CopyWebpackPlugin([
+			{ from: './src/content', to: 'content' }
+		])
+	],
+
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+
+	devServer: {
+		inline: true,
+		contentBase: './dist',
+		port: 8081
+	}
+};
